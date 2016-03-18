d46886b9e3f8b06446a3e85e15b121fa353c843e
leogatgens <leogatgens@gmail.com>
2016-03-18 11:32:44 -0600

    Se agrega bootstrap template

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 
 var port = '5000';
+
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+    res.send('Hello Leo como estas?');
+});
+
+
 app.listen(5000, function (err) {
     console.log('running server on port ' + port);
 });
