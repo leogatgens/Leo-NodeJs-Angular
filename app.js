405aab820c2a54d6e4a560963795a96166f593ce
Leonardo Melendez <leogatgens@gmail.com>
2016-03-21 22:36:46 -0600

    cambios logo
    se redujo el tamano del lgo

--- app.js
+++ app.js
@@ -15,10 +15,3 @@ app.get('/', function (req, res) {
 app.listen(port, function (err) {
     console.log('running server on port ' + port);
 });
-/*
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);*/
